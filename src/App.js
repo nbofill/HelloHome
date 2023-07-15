4b13d1a857ad66ff2d5ac94d02dce358231335a4
nbofill <nico.bofill1@gmail.com>
2023-07-15 16:23:08 -0400

    add picture and search func

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import FormItem from 'antd/es/form/FormItem';
 import Input from 'antd/es/input/Input';
 import { Button } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-import { Layout } from 'antd';
+import { Layout, Image } from 'antd';
 
 
 const { Content } = Layout;
@@ -14,16 +14,22 @@ const contentStyle = {
   height: "100vh", 
 }
 
+const onReset = () => {
+  document.getElementById('searchForm').reset();
+}
+
 function App() {
   return (
         <Content style={contentStyle}>
           <h2>Begin your search for a new Home</h2>
-          <Form style={{ marginLeft: '5%', marginRight: '5%'}}>
+          <Image width={200} src="HelloHome/public/favicon.png"></Image>
+          <Form id='searchForm' style={{ marginLeft: '15%', marginRight: '15%'}}>
             <FormItem name='city' rules={[{ required: true, message: 'Please fill this field' }]}><Input size='large' placeholder='City'></Input></FormItem>
             <FormItem name='state' rules={[{ required: true, message: 'Please fill this field' }]}><Input size='large' placeholder='State'></Input></FormItem>
             <FormItem name='min'><Input size='large' placeholder='Min Price($)'></Input></FormItem>
             <FormItem name='max'><Input size='large' placeholder='Max Price($)'></Input></FormItem>
-            <Button type='primary' icon={<SearchOutlined />} size='large' htmlType='submit'>Search for Homes</Button>
+            <Button style={{marginRight: '2%'}} type='primary' icon={<SearchOutlined />} size='large' htmlType='submit'>Search for Homes</Button>
+            <Button type='default' size='large' onClick={onReset}>Clear</Button>
           </Form>
         </Content>
         
