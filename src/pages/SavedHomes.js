3f8875a333ca389ef103223e1c057e5e28a0e6d4
nbofill <nico.bofill1@gmail.com>
2023-07-15 16:25:26 -0400

    add buttons and filter func

--- src/pages/SavedHomes.js
+++ src/pages/SavedHomes.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import HeaderTemp from "../elements/HeaderTemp";
 import SiderTemp from "../elements/SiderTemp";
 import Layout, { Content } from 'antd/es/layout/layout';
+import Form from 'antd/es/form/Form';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
     <React.StrictMode>
         <Layout>
@@ -11,7 +13,9 @@ root.render(
             <Layout hasSider>
                 <SiderTemp></SiderTemp>
                 <Content>
-                
+                <Form layout='inline'>
+                    <FormItem> </FormItem>
+                </Form>
                 </Content>
             </Layout>
         </Layout>
