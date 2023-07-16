8371f67901e1e28ecf1d8a0732a026e6f1132772
nbofill <nico.bofill1@gmail.com>
2023-07-16 19:53:29 -0400

    add filter func

--- src/pages/SavedHomes.js
+++ src/pages/SavedHomes.js
@@ -4,7 +4,11 @@ import HeaderTemp from "../elements/HeaderTemp";
 import SiderTemp from "../elements/SiderTemp";
 import Layout, { Content } from 'antd/es/layout/layout';
 import Form from 'antd/es/form/Form';
-import { Input } from 'antd';
+import { Input, Button } from 'antd';
+
+const onReset = () => {
+    document.getElementById('filterForm').reset();
+  }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,13 +18,15 @@ root.render(
             <Layout hasSider>
                 <SiderTemp></SiderTemp>
                 <Content>
-                <Form layout='inline'>
-                    <FormItem>City <Input /></FormItem>
-                    <FormItem>State <Input /></FormItem>
-                    <FormItem>Price <Input /></FormItem>
-                    <FormItem>Bed <Input /></FormItem>
-                    <FormItem>Bath <Input /></FormItem>
-                    <FormItem>Sqft <Input /></FormItem>
+                <Form id='filterForm' layout='inline' style={{marginLeft: '2%', marginTop: '1%'}}>
+                    <FormItem name='city'>City <Input /></FormItem>
+                    <FormItem name='state'>State <Input /></FormItem>
+                    <FormItem name='price'>Price <Input /></FormItem>
+                    <FormItem name='bed'>Bed <Input /></FormItem>
+                    <FormItem name='bath'>Bath <Input /></FormItem>
+                    <FormItem name='sqft'>Sqft <Input /></FormItem>
+                    <Button type='primary' style={{marginTop: '1.25%', marginRight: '1%'}}>Filter</Button>
+                    <Button type='default' onClick={onReset}  style={{marginTop: '1.25%'}}>Clear</Button>
                 </Form>
                 </Content>
             </Layout>
